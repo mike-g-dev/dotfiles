0eb424e0e3a2fdb55a6d7d7e96d9e4a67e28fda8
Mike Garrett <mikegdevemail@gmail.com>
2023-08-04 13:35:09 -0400

    Added patterns for telescope to ignore

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = {
+      'venv', '__pycache__'
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -28,4 +31,3 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-
