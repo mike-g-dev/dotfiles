e61d8cc447f5ebf538474b7d3e100bfd2bd6092d
Mike Garrett <mikegdevemail@gmail.com>
2023-10-04 16:51:47 -0400

    Migrated from ibl v2 to v3

--- nvim/.config/nvim/after/plugin/indent_blankline.lua
+++ nvim/.config/nvim/after/plugin/indent_blankline.lua
@@ -1,11 +1,10 @@
 -- See `:help indent_blankline.txt`
-local status, indent_blankline = pcall(require, 'indent_blankline')
+local status, indent_blankline = pcall(require, 'ibl')
 if not status then
   print('indent_blankline not installed')
   return
 end
 
 indent_blankline.setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
+  indent = { char = '┊' },
 }
