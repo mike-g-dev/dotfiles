8516eaab96edc5ff536f5773bccbcfdc71784b09
Mike Garrett <mikegdevemail@gmail.com>
2023-12-15 19:31:40 -0500

    Refactored treesitter and lualine loading into Lazy

--- nvim/.config/nvim/after/plugin/lualine.lua
+++
@@ -1,9 +0,0 @@
--- See `:help lualine.txt`
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'tokyonight',
-    component_separators = '|',
-    section_separators = '',
-  },
-}
