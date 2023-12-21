eadd05138d72e22bfda39c654c0e3d6aa1b9a1f3
Mike Garrett <mikegdevemail@gmail.com>
2023-12-21 18:40:32 -0500

    Refactored telescope

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -274,84 +274,25 @@ return {
 				indent = { char = '┊' },
 			}
 		end
-	}, -- Add indentation guides even on blank lines
+	},
 	{
 		'numToStr/Comment.nvim',
 		config = function()
 			require('Comment').setup()
 		end
-	},               -- "gc" to comment visual regions/lines
-	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		},
-		config = function()
-			require('telescope').setup {
-				defaults = {
-					file_ignore_patterns = {
-						'venv', '__pycache__'
-					},
-					mappings = {
-						i = {
-							['<C-u>'] = false,
-							['<C-d>'] = false,
-						},
-					},
-					layout_strategy = 'vertical',
-				},
-			}
-
-			-- Enable telescope fzf native, if installed
-			pcall(require('telescope').load_extension, 'fzf')
-
-			-- See `:help telescope.builtin`
-			vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
-				{ desc = '[?] Find recently opened files' })
-			vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
-				{ desc = '[ ] Find existing buffers' })
-			vim.keymap.set('n', '<leader>/', function()
-				-- You can pass additional configuration to telescope to change theme, layout, etc.
-				require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes')
-					.get_dropdown {
-						winblend = 10,
-						previewer = false,
-					})
-			end, { desc = '[/] Fuzzily search in current buffer]' })
-
-			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,
-				{ desc = '[S]earch [F]iles' })
-			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,
-				{ desc = '[S]earch [H]elp' })
-			vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
-				{ desc = '[S]earch current [W]ord' })
-			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,
-				{ desc = '[S]earch by [G]rep' })
-			vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
-				{ desc = '[S]earch [D]iagnostics' })
-		end
 	},
-	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build =
-		'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-	},
-	-- File tree
+	'tpope/vim-sleuth',
 	{
 		'nvim-tree/nvim-tree.lua',
 		config = function()
 			require("nvim-tree").setup()
 		end
 	},
-	-- Tmux
 	{
 		'alexghergh/nvim-tmux-navigation',
 		config = function()
 			require 'nvim-tmux-navigation'.setup {
-				disable_when_zoomed = true, -- defaults to false
+				disable_when_zoomed = true,
 				keybindings = {
 					left = "<C-h>",
 					down = "<C-j>",
