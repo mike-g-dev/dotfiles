45764ba118868000fcbd2501fc0adf76966e99ed
Mike Garrett <mikegdevemail@gmail.com>
2024-02-26 15:50:14 -0500

    Added zig support

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -29,6 +29,7 @@ return {
         "html",
         "cssls",
         "tailwindcss",
+        "zls"
       },
     })
 
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
         ensure_installed = {
           "c",
           "cpp",
+          "zig",
           "go",
           "lua",
           "python",
