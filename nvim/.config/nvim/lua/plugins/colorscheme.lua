df83d953ef6f7e8d68955f3164879febf8e3873e
Mike Garrett <mikegdevemail@gmail.com>
2024-03-02 22:31:14 -0500

    Configured onedark

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,44 @@
 return {
-	-- {
-	-- 	'folke/tokyonight.nvim',
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require('tokyonight').setup({
-	-- 			style = 'moon',
-	-- 			transparent = true,
-	-- 		})
-	-- 		vim.cmd [[colorscheme tokyonight]]
-	-- 	end,
-	-- },
-	{
-		'mellow-theme/mellow.nvim',
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- vim.g.mellow_bold_functions = true
-			-- vim.g.mellow_bold_variables = true
-			vim.g.mellow_bold_keywords = true
-			vim.g.mellow_transparent = true
-			vim.cmd [[colorscheme mellow]]
-		end
-	},
-	'navarasu/onedark.nvim',
-	'craftzdog/solarized-osaka.nvim'
+  -- {
+  -- 	'folke/tokyonight.nvim',
+  -- 	lazy = false,
+  -- 	priority = 1000,
+  -- 	config = function()
+  -- 		require('tokyonight').setup({
+  -- 			style = 'moon',
+  -- 			transparent = true,
+  -- 		})
+  -- 		vim.cmd [[colorscheme tokyonight]]
+  -- 	end,
+  -- },
+  {
+    "mellow-theme/mellow.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.g.mellow_bold_functions = true
+      -- vim.g.mellow_bold_variables = true
+      vim.g.mellow_bold_keywords = true
+      vim.g.mellow_transparent = true
+      vim.cmd([[colorscheme mellow]])
+    end,
+  },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("onedark").setup({
+  --       style = "deep",
+  --       transparent = true,
+  --       code_style = {
+  --         comments = "italic",
+  --         keywords = "bold",
+  --         functions = "bold",
+  --       },
+  --     })
+  --     vim.cmd([[colorscheme onedark]])
+  --   end,
+  -- },
+  "craftzdog/solarized-osaka.nvim",
 }
