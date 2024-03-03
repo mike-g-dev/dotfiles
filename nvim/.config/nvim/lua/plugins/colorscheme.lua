6805f3033c6f7449e091a294a6601bb389a5b370
Mike Garrett <mikegdevemail@gmail.com>
2024-03-03 10:26:50 -0500

    Added which-key, todo-comments, and changed colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,34 +11,34 @@ return {
   -- 		vim.cmd [[colorscheme tokyonight]]
   -- 	end,
   -- },
-  {
-    "mellow-theme/mellow.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- vim.g.mellow_bold_functions = true
-      -- vim.g.mellow_bold_variables = true
-      vim.g.mellow_bold_keywords = true
-      vim.g.mellow_transparent = true
-      vim.cmd([[colorscheme mellow]])
-    end,
-  },
   -- {
-  --   "navarasu/onedark.nvim",
+  --   "mellow-theme/mellow.nvim",
   --   lazy = false,
   --   priority = 1000,
   --   config = function()
-  --     require("onedark").setup({
-  --       style = "deep",
-  --       transparent = true,
-  --       code_style = {
-  --         comments = "italic",
-  --         keywords = "bold",
-  --         functions = "bold",
-  --       },
-  --     })
-  --     vim.cmd([[colorscheme onedark]])
+  --     -- vim.g.mellow_bold_functions = true
+  --     -- vim.g.mellow_bold_variables = true
+  --     vim.g.mellow_bold_keywords = true
+  --     vim.g.mellow_transparent = true
+  --     vim.cmd([[colorscheme mellow]])
   --   end,
   -- },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "deep",
+        transparent = true,
+        code_style = {
+          comments = "italic",
+          keywords = "bold",
+          functions = "bold",
+        },
+      })
+      vim.cmd([[colorscheme onedark]])
+    end,
+  },
   "craftzdog/solarized-osaka.nvim",
 }
