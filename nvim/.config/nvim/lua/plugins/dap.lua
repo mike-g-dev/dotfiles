3270485f4e8da4bfa4051af930ae2cab562d51d2
Mike Garrett <mikegdevemail@gmail.com>
2024-08-01 20:00:28 -0400

    Hacked python dap...

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -13,7 +13,10 @@ return {
     dapui.setup()
 
     -- TODO: realistically, I would like to configure LSP and DAP for each language I use in one place...
-    local python = os.getenv("VIRTUAL_ENV") .. "/bin/python"
+    -- TODO: this also loads when you open neovim and makes no sense
+    -- unless we are in a python project...
+    local python = vim.fn.getcwd() .. "/venv/bin/python"
+    print(python)
     require("dap-python").setup(python)
 
     dap.listeners.before.attach.dapui_config = function()
