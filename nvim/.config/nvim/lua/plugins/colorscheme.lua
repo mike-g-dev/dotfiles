9553ca2a4b61975858ffc41c9805c4dd601d5139
Mike Garrett <mikegdevemail@gmail.com>
2025-01-20 19:04:21 -0500

    kanagawa and trouble setup

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,6 +11,44 @@ return {
   --     vim.cmd([[colorscheme tokyonight]])
   --   end,
   -- },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { bold = true },
+        statementStyle = { bold = true },
+        typeStyle = { bold = true },
+        transparent = true, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = { }, lotus = {}, dragon = {}, all = { ui = { bg_gutter = "none" } } },
+        },
+        overrides = function(colors) -- add/modify highlights
+          local theme = colors.theme
+          return {
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          }
+        end,
+        theme = "wave",
+      })
+
+      vim.cmd([[colorscheme kanagawa]])
+    end,
+  },
   -- {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
@@ -47,23 +85,23 @@ return {
   --     vim.cmd([[colorscheme vague]])
   --   end,
   -- },
-  {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("onedark").setup({
-        style = "warm",
-        transparent = true,
-        code_style = {
-          comments = "italic",
-          keywords = "bold",
-          functions = "bold",
-        },
-      })
-      vim.cmd([[colorscheme onedark]])
-    end,
-  },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("onedark").setup({
+  --       style = "warm",
+  --       transparent = true,
+  --       code_style = {
+  --         comments = "italic",
+  --         keywords = "bold",
+  --         functions = "bold",
+  --       },
+  --     })
+  --     vim.cmd([[colorscheme onedark]])
+  --   end,
+  -- },
   -- {
   --   "craftzdog/solarized-osaka.nvim",
   --   lazy = false,
