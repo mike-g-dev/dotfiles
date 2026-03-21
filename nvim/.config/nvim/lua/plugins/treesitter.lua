70e4cd7b45200d290a07973d9242a71056b8a6d3
Mike Garrett <mikegdevemail@gmail.com>
2026-03-21 13:30:06 -0400

    feat: Fixed treesitter config and improved starship prompt

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,44 +2,46 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    tag = "v0.10.0",
     lazy = false,
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c",
-          "cpp",
-          "zig",
-          "go",
-          "lua",
-          "python",
-          "rust",
-          "typescript",
-          "javascript",
-          "vim",
-          "vimdoc",
-          "nix",
-          "ocaml",
-          "bash",
-          "swift",
-          "terraform",
-          "hcl",
-          "yaml",
-          "json",
-          "dockerfile",
-          "gitignore",
-          "markdown",
-          "html",
-          "lua",
-          "make",
-          "proto",
-          "toml",
-          "sql",
-          "cue",
-        },
-        highlight = {
-          enable = true, -- Enable syntax highlighting
-        },
+      local parsers = {
+        "c",
+        "cpp",
+        "zig",
+        "go",
+        "lua",
+        "python",
+        "rust",
+        "typescript",
+        "javascript",
+        "vim",
+        "vimdoc",
+        "nix",
+        "ocaml",
+        "bash",
+        "swift",
+        "terraform",
+        "hcl",
+        "yaml",
+        "json",
+        "dockerfile",
+        "gitignore",
+        "markdown",
+        "html",
+        "make",
+        "proto",
+        "toml",
+        "sql",
+        "cue",
+      }
+
+      require("nvim-treesitter").install(parsers)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = parsers,
+        callback = function()
+          vim.treesitter.start()
+        end,
       })
     end,
   },
